b5d6e2e4e1f266d5a5d682fad2a193e1341b2800
kuroneko1202 <91447097+kuroneko1202@users.noreply.github.com>
2024-11-14 13:21:50 +0000

    lesson8_8

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -5,8 +5,17 @@ def get_names(nums:int=2)->list[str]:
     names:list[str] = names_str.split(sep='\n')
     names = random.choices(names,k=nums)
     return names
+def generate_students(names:list[str]) -> list[dict]:
+    students:list[dict] = []
+    for name in names:
+        chinese = random.randint(50, 100)
+        english = random.randint(50, 100)
+        math = random.randint(50, 100)
+        student = {'name':name,'chinese':chinese,'english':english,'math':math}
+        students.append(student)
+    return students  
 
 nums = int(input("請輸入學生數量(最多10位):"))
 student_names:list[str] = get_names(nums=nums)
-for name in student_names:
-    print(name)
+students:list[dict] = generate_students(names=student_names)
+print(students)
